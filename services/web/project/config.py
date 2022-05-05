4ee9b92fd1e2c9719ee3894a2469a03c6dd0beec
ilyas <ilyas@ticdoor.com>
2022-05-05 15:38:39 +0300

    condig file check

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -9,8 +9,6 @@ class Config(object):
     """Base config vars."""
     ROOT_PATH = basedir
     SECRET_KEY = '9OLWxND4o83j4Kty567pO'
-    ADM_USERNAME = 'admin'
-    ADM_PASSWORD = 'default'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite?check_same_thread=False')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
